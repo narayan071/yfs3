f0e1502a00754996d51805148f94f31dc56fdd54
Bhakti Prakash Das <prakashbhakti2@gmail.com>
2023-08-12 11:22:46 +0530

    setting env

--- yfs3/settings.py
+++ yfs3/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # created apps
     'home',
 ]
 
@@ -121,8 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+# Base url to serve media files
+MEDIA_URL = '/media/'
+# Path where media is stored'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
