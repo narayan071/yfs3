5e3141913d7b19ec7ddd6f3a3ed2cdcdc30132e5
Narayan Pradhan <narayan.pradhan1117@gmail.com>
2023-08-15 12:32:16 +0530

    added events page

--- events/views.py
+++ events/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+
+def events(request):
+    context = {
+        'pname' : 'events',
+    }
+    return render(request, 'events/events.html', context)
