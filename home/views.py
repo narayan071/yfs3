773e69189833ae21b419907a4542954cf0e77ebb
Bhakti Prakash Das <prakashbhakti2@gmail.com>
2023-08-22 20:56:15 +0530

    url mapping

--- home/views.py
+++ home/views.py
@@ -6,3 +6,13 @@ def home(request):
         'pname':"home",
     }
     return render(request, 'home/home.html', context)
+def contact(request):
+    context={
+        "pname":"contact"
+    }
+    return render(request,'home/contact.html',context)
+def feedback(request):
+    context={
+        "pname":"feedback"
+    }
+    return render(request,'home/feedback.html',context)
