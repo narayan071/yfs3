2254ab08f6149a4cb21b659cd012f835c1fc6c91
Narayan Pradhan <narayan.pradhan1117@gmail.com>
2023-08-23 18:38:51 +0530

    login, signup page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def login(request):
+    context={
+        "pname": "login",
+    }
+
+    return render(request, "user/login.html", context)
+
+def signup(request):
+    context={
+        "pname": "signup",
+    }
+
+    return render(request, "user/signup.html", context)
--- yfs3/urls.py
+++ yfs3/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('events/', include('events.urls')),
     path('blogs/', include('blog.urls')),
     path('joinus/', include('joinus.urls')),
+    path('user/', include('accounts.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
