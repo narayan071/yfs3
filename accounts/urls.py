75f7c401e9d07347ce93951f15b5078b9f0352ad
Bhakti Prakash Das <prakashbhakti2@gmail.com>
2023-08-30 00:41:31 +0530

    init profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("signin/", views.signin, name="signin"),
     path("signup/", views.signup, name="signup"),
     path("signout/",views.signout, name="signout" ),
-
+    path("profile/",views.profile,name="profile")
 ]
