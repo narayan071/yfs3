3046e2c8b0847bd1b88d714e5d058a910dc6206a
Narayan Pradhan <narayan.pradhan1117@gmail.com>
2023-09-09 14:42:14 +0530

    designs

--- chapters/urls.py
+++ chapters/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.chapters, name="chapters"),
     path('new-chapter/', views.new_chapter, name="new-chapter"),
     path('join-chapter/', views.join_chapter, name="join-chapter"),
+    path('join-us/', views.joinus, name="joinus"),
     path('chapter/<int:chapter_id>/', views.chapterdetails, name="chapterdetails"),
 ]
