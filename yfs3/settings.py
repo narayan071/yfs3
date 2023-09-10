30dcf82e07e4c1b90ee5271335a5ab3f15b54395
Narayan Pradhan <narayan.pradhan1117@gmail.com>
2023-09-10 11:40:03 +0530

    database changed

--- yfs3/settings.py
+++ yfs3/settings.py
@@ -103,10 +103,15 @@ WSGI_APPLICATION = 'yfs3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'YFS',
+        'USER': 'root',
+        'PASSWORD': 'pass1111',
+        'HOST': 'localhost',  # If your database is on a different host, specify it here
+        'PORT': '3306',           # Leave empty to use the default MySQL port (3306)
     }
 }
+
 
 
 # Password validation
