9ae12fced43cb5e0aaade48a0b4cb91322845b67
Narayan Pradhan <narayan.pradhan1117@gmail.com>
2023-09-26 23:08:27 +0530

    added team

--- yfs3/settings.py
+++ yfs3/settings.py
@@ -105,24 +105,24 @@ WSGI_APPLICATION = 'yfs3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'YFS',
-#         'USER': 'root',
-#         'PASSWORD': 'pass1111',
-#         'HOST': 'localhost',  
-#         'PORT': '3306',           
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'YFS',
+        'USER': 'root',
+        'PASSWORD': 'pass1111',
+        'HOST': 'localhost',  
+        'PORT': '3306',           
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
